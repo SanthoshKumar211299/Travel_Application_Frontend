bb4cc68eb32c7d489907197cb30c49fa0603408f
SanthoshKumar211299 <subjansan@gmail.com>
2024-08-28 14:09:19 +0530

    changes added

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,9 +5,11 @@ const About = () => {
   return <section>
     <Container>
         <Row>
-            <Col>
-            <h1>About SandyTravels</h1>
-            <p><span>SandyTravels</span> is your go-to travel companion designed to simplify your journey from start to finish. With [unique feature or value proposition], we make it easy for you to [primary benefit, e.g., find the best travel deals, discover hidden gems, or plan your perfect trip]. Our mission is to enhance your travel experience with [additional feature or benefit, e.g., personalized recommendations, seamless booking, or real-time updates], ensuring every trip is memorable and hassle-free.</p>
+            <Col lg='8'>
+            <div className='about__content'>
+            <h1>About SandyTravels </h1>
+            <p><span className='highlight'>SandyTravels </span> is your go-to travel companion designed to simplify your journey from start to finish. With [unique feature or value proposition], we make it easy for you to [primary benefit, e.g., find the best travel deals, discover hidden gems, or plan your perfect trip]. Our mission is to enhance your travel experience with [additional feature or benefit, e.g., personalized recommendations, seamless booking, or real-time updates], ensuring every trip is memorable and hassle-free.</p>
+            </div>
             </Col>
         </Row>
     </Container>
