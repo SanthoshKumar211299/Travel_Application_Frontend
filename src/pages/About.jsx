4c50334d493a08cf24f318823ca779b73bebf74f
SanthoshKumar211299 <subjansan@gmail.com>
2024-08-28 13:11:50 +0530

    about page changes

---
+++ src/pages/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Col, Container } from 'reactstrap'
+
+const About = () => {
+  return <section>
+    <Container>
+        <Row>
+            <Col>
+            <h1>About SandyTravels</h1>
+            <p>Brief overview of the travel app and what it offers</p>
+            </Col>
+        </Row>
+    </Container>
+  </section>
+}
+
+export default About
